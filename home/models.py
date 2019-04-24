721f28bf2998dd9e421615250873ef52cf14583a
nemqueteba <nemqueteba@nabusimaque.mygateway.net>
2019-04-24 01:07:05 -0500

    Home model update...
    Committer: opensai <academic@opensai.org>

--- home/models.py
+++ home/models.py
@@ -1,7 +1,24 @@
+#from django.db import models
+
+#from wagtail.core.models import Page
+
+
+#class HomePage(Page):
+#    pass
+
+
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+
