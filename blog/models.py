4811f5cd1987e927b9cf0f1c35159ccd7830965c
nemqueteba <nemqueteba@nabusimaque.mygateway.net>
2019-04-29 23:53:20 -0500

    Update wagtail to V 2.5 ...create blog app

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+# from django.db import models
+
+# Create your models here.
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
